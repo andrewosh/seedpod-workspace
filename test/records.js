72786784754bf968eec8991b6e4e7430cec4c3d3
Andrew Osheroff <andrewosh@gmail.com>
2018-07-28 18:01:34 -0700

    Cleaned up package creation

--- test/records.js
+++ test/records.js
@@ -11,9 +11,8 @@ const create = require('./helpers/create')
 
 const PACKAGE_ROOT = p.join(__dirname, 'data', 'packages')
 
-
 test('can insert a single record for a simple type', async t => {
-  let [client, handle] = await registerAndBind('location', 'location-tagger')
+  let [client, handle] = await registerAndBind('location-tagger', 'Location')
 
   let doc = {
     coords: {
@@ -33,7 +32,7 @@ test('can insert a single record for a simple type', async t => {
 })
 
 test('can insert and get a single record for a simple type by ID (no revision)', async t => {
-  let [client, handle] = await registerAndBind('location', 'location-tagger')
+  let [client, handle] = await registerAndBind('location-tagger', 'Location')
 
   let doc = {
     coords: {
@@ -47,9 +46,6 @@ test('can insert and get a single record for a simple type by ID (no revision)',
   t.true(_id)
   t.true(_revs[0])
 
-  console.log('GETTING DOC WITH ID:', _id)
-  console.log('  AND REVS IS:', _revs)
-
   let docs = await get(t, client, _id)
   t.same(docs.values.length, 1)
   doc = docs.values[0]
@@ -58,6 +54,50 @@ test('can insert and get a single record for a simple type by ID (no revision)',
   t.same(doc.id._revs, _revs)
   t.same(doc.value.name, 'Home')
   t.ok(almostEqual(doc.value.coords.lat, 34.168704, almostEqual.FLT_EPSILON))
+
+  await handle.close()
+  t.end()
+})
+
+test.skip('can insert and get a complicated record with nested types', async t => {
+  let [client, handle] = await registerAndBind('dogs', 'Walker')
+
+  let doc = {
+    age: {
+      birthday: '9/28/1990',
+      age: 27
+    },
+    walking: [
+      {
+        breed: {
+          name: 'Pug',
+          // TODO: get the protobuf types for enum references etc
+          energy: 1
+        },
+        name: 'Evan',
+        limbCount: 4,
+        populationCount: 40000
+      }
+    ]
+  }
+
+  let [_id, _revs] = await insert(t, client, doc)
+  t.true(_id)
+  t.true(_revs[0])
+
+  let docs = await get(t, client, _id)
+  t.same(docs.values.length, 1)
+  doc = docs.values[0]
+
+  t.same(doc.id._id, _id)
+  t.same(doc.id._revs, _revs)
+  let value = doc.value
+  t.same(value.age.age, 27)
+  t.same(value.age.birthday, '9/28/1990')
+  t.same(value.walking.length, 1)
+  t.same(value.walking[0].breed.name, 'Pug')
+  t.same(value.walking[0].breed.energy, 1)
+  t.same(value.walking[0].limbCount, 4)
 
   await handle.close()
   t.end()
@@ -110,22 +150,40 @@ async function get (t, client, _id, _revs) {
   })
 }
 
-async function registerAndBind (dir, packageName) {
-  let [ appDb ] = await create.fromPackages([
-    p.join(PACKAGE_ROOT, dir)
-  ])
-  await appDb.publish('v1')
+async function createAndPublish (name) {
+  let manifest = JSON.parse(await fs.readFile(p.join(PACKAGE_ROOT, name, 'manifest.json'), 'utf8'))
+  let iface = await fs.readFile(p.join(PACKAGE_ROOT, name, 'interface.spdl'), 'utf8')
+  let deps = manifest.dependencies
+  if (deps) {
+    for (let depName of Object.keys(deps)) {
+      let { db } = await createAndPublish(depName)
+      deps[depName].key = datEncoding.encode(db.key)
+    }
+  }
+
+  let appDb = await create.one()
+  console.log('updating package with iface', iface, 'manifest:', manifest)
+  await appDb.updatePackage(iface, manifest)
+  await appDb.publish(manifest.version, { skipVersioning: true })
+
+  return {
+    db: appDb,
+    version: manifest.version
+  }
+}
+
+async function registerAndBind (packageName, type) {
+  let { db: appDb, version } = await createAndPublish(packageName)
   let key = datEncoding.encode(appDb.key)
 
   let packageDb = await create.one()
-  await packageDb.install(key, 'v1')
+  await packageDb.install(key, version)
   let { proto } = await appDb.packages.export()
-  console.log('PROTO:', proto)
 
-  let handle = await packageDb.bind(packageName, 'v1')
+  let handle = await packageDb.bind(packageName, version)
 
   let root = await createRootClient(proto)
-  let client = new root.Location(`localhost:${handle.port}`, grpc.credentials.createInsecure())
+  let client = new root[type](`localhost:${handle.port}`, grpc.credentials.createInsecure())
 
   return [client, handle]
 }
